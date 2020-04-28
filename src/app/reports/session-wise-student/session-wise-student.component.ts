8ce04449f2403773784a9b1f772595bfefb57df6
Aashray Jain <aashrayjainkatni@gmail.com>
2020-04-28 15:55:37 +0530

    session-wise-student form created

--- src/app/reports/session-wise-student/session-wise-student.component.ts
+++ src/app/reports/session-wise-student/session-wise-student.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Subject } from 'rxjs';
+import { DataTableDirective } from 'angular-datatables';
+import { Session } from 'src/app/session/session.component';
+import { Course } from 'src/app/course/course.component';
+import { Branch } from 'src/app/branches/branches.component';
+
+export class SessionWiseStudent {
+  public constructor(
+    public session: Session, public branchName: String, course: Course
+  ) {
+
+  }
+}
 
 @Component({
   selector: 'app-session-wise-student',
@@ -7,9 +20,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SessionWiseStudentComponent implements OnInit {
 
+
+  sessionList: Session[];
+  branches: Branch[];
+  courseName: String;
+
+  dtTrigger: Subject<any> = new Subject();
+  @ViewChild(DataTableDirective, { static: false })
+  datatableElement: DataTableDirective;
+  dtOptions: DataTables.Settings = {};
+
   constructor() { }
 
   ngOnInit() {
+    this.sessionList = [new Session(1, "abc", new Date(), new Date(), 1, new Date(), 54, new Date()), new Session(1, "abc", new Date(), new Date(), 1, new Date(), 54, new Date()),
+    new Session(1, "abc", new Date(), new Date(), 1, new Date(), 54, new Date())]
+    this.branches = [
+        new Branch(1, "M.P. NAGAR", "mpn", "3rd Floor, 275", "above Andhra Bank, Near Time Coaching", "Zone-II, Maharana Pratap Nagar", "Bhopal", " Madhya Pradesh",["Rohit Ahuja"],"1234556778","JAVA,C++,C",new Date(),"23.2524° N, 77.4646° E"),
+        new Branch(2, "Indrapuri", "ind", "219-B", " near Gaurav Dairy, Indrapuri C sector", "Sector C, Indrapuri", "Bhopal", "Madhya Pradesh",["Rohit Ahuja"],"1234556778","JAVA,C++,C",new Date(),"23.2524° N, 77.4646° E")
+      ]
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      lengthMenu: [5, 10, 15, 20],
+    };
+  }
+
+  getStudentByCourseName() {
+    
   }
 
 }
