99b498c5ac59ba3153dbf648f8e1c285d65a573b
stark2710 <bharatmalviya1999@gmail.com>
2020-05-06 11:26:35 +0530

    Student Data Table

---
+++ src/app/service/data/student-data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { API_URL } from 'src/app/app.constants';
+import { Student } from 'src/app/student/student.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentDataService {
+
+  constructor(private http : HttpClient) { }
+
+  getAllStudents(){
+    return this.http.get<Student[]>(`${API_URL}/vedisoft/students`)
+  }
+
+  getStudentById(id){
+    return this.http.get<Student>(`${API_URL}/vedisoft/student/${id}`)
+  }
+
+  addNewStudent(student){
+    return this.http.post(`${API_URL}/vedisoft/student/formSubmit`,student)
+  }
+
+  updateStudent(student,id){
+    return this.http.put(`${API_URL}/vedisoft/student/formEdit/${id}`,student)
+  }
+}
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -3,14 +3,40 @@ import { Subject } from 'rxjs';
 import { DataTableDirective, DataTablesModule } from 'angular-datatables';
 import { Branch } from 'src/app/branches/branches.component';
 import { College } from 'src/app/college/college.component';
+import { Router } from '@angular/router';
+import { StudentDataService } from '../service/data/student-data.service';
 
 export class Student {
-  constructor(public registrationId: number, public branch: string, public campus: string, public cardNo: number,
-    public company: string, public course: string, public createdBy: number, public creationDate: Date, public dob: Date, 
-    public doj: Date, public email: string, public enquiry_id: number, public extra3: string, public fatherName: string, 
-    public flag: string, public laddress: string, public lastUpdadtedBy: number, public lastUpdationDate: Date, public name: string, 
-    public package1: string, public paddress:string, public parentOccupation: string, public password: string, public pno: string,
-    public regId: string, public registrationDate: Date, public sem: String, public whatsappNo: string, public branchBean: Branch,
+  constructor(public registrationId: number, 
+    public branch: string, 
+    public campus: string, 
+    public cardNo: number,
+    public company: string, 
+    public course: string, 
+    public createdBy: number, 
+    public creationDate: Date, 
+    public dob: Date, 
+    public doj: Date, 
+    public email: string, 
+    public enquiry_id: number, 
+    public extra3: string, 
+    public fatherName: string, 
+    public flag: string, 
+    public laddress: string, 
+    public lastUpdadtedBy: number, 
+    public lastUpdationDate: Date, 
+    public mno:number,
+    public name: string, 
+    public package1: string, 
+    public paddress:string, 
+    public parentOccupation: string, 
+    public password: string, 
+    public pno: string,
+    public regId: string, 
+    public registrationDate: Date, 
+    public sem: String, 
+    public whatsappNo: string, 
+    public branchBean: Branch,
     public collegeList: College) { }
 }
 
@@ -21,7 +47,7 @@ export class Student {
 })
 export class StudentComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(public router:Router , private studentData : StudentDataService) { }
 
   dtTrigger: Subject<any> = new Subject();
   @ViewChild(DataTableDirective, { static: false })
@@ -30,28 +56,33 @@ export class StudentComponent implements OnInit, OnDestroy {
   isEnabled: boolean = false;
   studentName = "";
 
-  students: Student[] = [
-    new Student(1, "Mp nagar", "yes", 825, "Infosys", "Java", 1, new Date(), new Date(), new Date(), "abc@gmail.com", 1, "", "Neelesh",
-    "", "MPNAGRA", 1, new Date(), "Aditya Kumar", "", "Bhopal", "Businessman", "123456", "1", "879346", new Date(), "4", "78654887685", 
-    new Branch(1, "MONAGRA", "MP", "54545", "MPNAGAR", "STREET", "BHOPAL", "MP", ["ABC"], "7384542", "JAVA", new Date(), "6455455"), 
-    new College(1, "BHOPAL", "LNCT", "LMCTS", "LN")),
-
-    new Student(2, "Indrapuri", "yes", 825, "Cognizant", "C++", 1, new Date(), new Date(), new Date(), "abc@gmail.com", 1, "", "Neelesh",
-    "", "MPNAGRA", 1, new Date(), "Divyanand Pandey", "", "Bhopal", "Businessman", "123456", "1", "879346", new Date(), "4", "78654887685", 
-    new Branch(1, "MONAGRA", "MP", "54545", "MPNAGAR", "STREET", "BHOPAL", "MP", ["ABC"], "7384542", "JAVA", new Date(), "6455455"), 
-    new College(1, "BHOPAL", "LNCT", "LMCTS", "LN", ))
-  ]
+  students: Student[]
 
   ngOnInit() {
-  }
-
-  searchStudent() {
+    
     this.dtOptions = {
       pagingType: 'full_numbers',
       lengthMenu: [5, 10, 15, 20],
       search: {search: this.studentName}
     };
     this.isEnabled = true;
+    this.refreshStudent()
+    this.dtTrigger
+  }
+
+  refreshStudent(){
+    this.studentData.getAllStudents().subscribe(
+      data=>{
+        this.students = data
+        this.dtTrigger.next()
+      },
+      error =>{
+        console.log(error)
+      }
+    )
+  }
+
+  searchStudent() {
   }
 
   ngOnDestroy() {
